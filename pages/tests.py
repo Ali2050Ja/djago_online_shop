9f3b40fdf9854680adb0b97d86dea47ddb8010f2
reza <rezajafarian723@gmail.com>
2022-08-13 07:30:18 -0700

    add crispy form bootstrap

--- pages/tests.py
+++ pages/tests.py
@@ -1,1 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class PagesTest(TestCase):
+
+    def test_home_page_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_url_by_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
